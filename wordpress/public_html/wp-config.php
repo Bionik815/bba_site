2f9e76c5bd3004b62fa33dbfa13ea0cc4cde9878
Nick <nhayes@Nicks-MacBook-Pro.local>
2026-03-14 12:59:12 -0700

    Allow local config overrides in wp-config

--- wordpress/public_html/wp-config.php
+++ wordpress/public_html/wp-config.php
@@ -27,16 +27,24 @@ define( 'WP_CACHE', true );
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv( 'WP_DB_NAME' ) ?: 'change_me_wp_db_name' );
+if ( ! defined( 'DB_NAME' ) ) {
+	define( 'DB_NAME', getenv( 'WP_DB_NAME' ) ?: 'change_me_wp_db_name' );
+}
 
 /** Database username */
-define( 'DB_USER', getenv( 'WP_DB_USER' ) ?: 'change_me_wp_db_user' );
+if ( ! defined( 'DB_USER' ) ) {
+	define( 'DB_USER', getenv( 'WP_DB_USER' ) ?: 'change_me_wp_db_user' );
+}
 
 /** Database password */
-define( 'DB_PASSWORD', getenv( 'WP_DB_PASSWORD' ) ?: 'change_me_wp_db_password' );
+if ( ! defined( 'DB_PASSWORD' ) ) {
+	define( 'DB_PASSWORD', getenv( 'WP_DB_PASSWORD' ) ?: 'change_me_wp_db_password' );
+}
 
 /** Database hostname */
-define( 'DB_HOST', getenv( 'WP_DB_HOST' ) ?: '127.0.0.1' );
+if ( ! defined( 'DB_HOST' ) ) {
+	define( 'DB_HOST', getenv( 'WP_DB_HOST' ) ?: '127.0.0.1' );
+}
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -55,15 +63,33 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',          getenv( 'WP_AUTH_KEY' ) ?: 'change_me_auth_key' );
-define( 'SECURE_AUTH_KEY',   getenv( 'WP_SECURE_AUTH_KEY' ) ?: 'change_me_secure_auth_key' );
-define( 'LOGGED_IN_KEY',     getenv( 'WP_LOGGED_IN_KEY' ) ?: 'change_me_logged_in_key' );
-define( 'NONCE_KEY',         getenv( 'WP_NONCE_KEY' ) ?: 'change_me_nonce_key' );
-define( 'AUTH_SALT',         getenv( 'WP_AUTH_SALT' ) ?: 'change_me_auth_salt' );
-define( 'SECURE_AUTH_SALT',  getenv( 'WP_SECURE_AUTH_SALT' ) ?: 'change_me_secure_auth_salt' );
-define( 'LOGGED_IN_SALT',    getenv( 'WP_LOGGED_IN_SALT' ) ?: 'change_me_logged_in_salt' );
-define( 'NONCE_SALT',        getenv( 'WP_NONCE_SALT' ) ?: 'change_me_nonce_salt' );
-define( 'WP_CACHE_KEY_SALT', getenv( 'WP_CACHE_KEY_SALT' ) ?: 'change_me_cache_key_salt' );
+if ( ! defined( 'AUTH_KEY' ) ) {
+	define( 'AUTH_KEY', getenv( 'WP_AUTH_KEY' ) ?: 'change_me_auth_key' );
+}
+if ( ! defined( 'SECURE_AUTH_KEY' ) ) {
+	define( 'SECURE_AUTH_KEY', getenv( 'WP_SECURE_AUTH_KEY' ) ?: 'change_me_secure_auth_key' );
+}
+if ( ! defined( 'LOGGED_IN_KEY' ) ) {
+	define( 'LOGGED_IN_KEY', getenv( 'WP_LOGGED_IN_KEY' ) ?: 'change_me_logged_in_key' );
+}
+if ( ! defined( 'NONCE_KEY' ) ) {
+	define( 'NONCE_KEY', getenv( 'WP_NONCE_KEY' ) ?: 'change_me_nonce_key' );
+}
+if ( ! defined( 'AUTH_SALT' ) ) {
+	define( 'AUTH_SALT', getenv( 'WP_AUTH_SALT' ) ?: 'change_me_auth_salt' );
+}
+if ( ! defined( 'SECURE_AUTH_SALT' ) ) {
+	define( 'SECURE_AUTH_SALT', getenv( 'WP_SECURE_AUTH_SALT' ) ?: 'change_me_secure_auth_salt' );
+}
+if ( ! defined( 'LOGGED_IN_SALT' ) ) {
+	define( 'LOGGED_IN_SALT', getenv( 'WP_LOGGED_IN_SALT' ) ?: 'change_me_logged_in_salt' );
+}
+if ( ! defined( 'NONCE_SALT' ) ) {
+	define( 'NONCE_SALT', getenv( 'WP_NONCE_SALT' ) ?: 'change_me_nonce_salt' );
+}
+if ( ! defined( 'WP_CACHE_KEY_SALT' ) ) {
+	define( 'WP_CACHE_KEY_SALT', getenv( 'WP_CACHE_KEY_SALT' ) ?: 'change_me_cache_key_salt' );
+}
 
 
 /**#@-*/
@@ -97,9 +123,15 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 	define( 'WP_DEBUG', false );
 }
 
-define( 'FS_METHOD', 'direct' );
-define( 'COOKIEHASH', getenv( 'WP_COOKIEHASH' ) ?: md5( (string) ( getenv( 'WP_DB_NAME' ) ?: 'local' ) ) );
-define( 'WP_AUTO_UPDATE_CORE', 'minor' );
+if ( ! defined( 'FS_METHOD' ) ) {
+	define( 'FS_METHOD', 'direct' );
+}
+if ( ! defined( 'COOKIEHASH' ) ) {
+	define( 'COOKIEHASH', getenv( 'WP_COOKIEHASH' ) ?: md5( (string) ( getenv( 'WP_DB_NAME' ) ?: 'local' ) ) );
+}
+if ( ! defined( 'WP_AUTO_UPDATE_CORE' ) ) {
+	define( 'WP_AUTO_UPDATE_CORE', 'minor' );
+}
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
